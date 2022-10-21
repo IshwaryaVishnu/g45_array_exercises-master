ae5ed22be1b0c4eadfa3c31ec7d8ec4608f9d6ec
Iswarya Devi Gomathi Sankar <ishu27.v@gmail.com>
2022-10-21 19:01:49 +0200

    Final commit

--- src/main/java/se/lexicon/Exercise13.java
+++ src/main/java/se/lexicon/Exercise13.java
@@ -1,5 +1,5 @@
 package se.lexicon;
-
+import java.util.Arrays;
 public class Exercise13 {
 
   /**
@@ -9,16 +9,22 @@ public class Exercise13 {
    * array and the odd numbers are located in the front part (the left side).
    */
   public static void ex13() {
-    int randomNumbers;
-
-    int arr[] = {1, 2, 4, 7, 9, 12};
-    System.out.println("Odd Numbers:");
-    for (int i = 0; i < arr.length; i++) {
-      if (arr[i] % 2 != 0) {
-        System.out.print(arr[i] + " \t");
-
+    int array1[] = {2, 4, 5, 1, 7, 8, 9, 7};
+    int arr2 = array1.length;
+    getArrangedEvenOdd(array1, arr2);
+  }
+  public static void getArrangedEvenOdd(int array1[], int array2) {
+    int i = -1, j = 0;
+    while (j != array2) {
+      if (array1[j] % 2 == 0) {
+        i++;
+        int array3 = array1[i];
+        array1[i] = array1[j];
+        array1[j] = array3;
       }
-
+      j++;
     }
+    for (int k = 0; k < array2; k++)
+      System.out.print(array1[k] + " ");
   }
 }
