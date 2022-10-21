a9dc3954b88b58c2b80d6efccbdde9d5cf8e7cf0
Iswarya Devi Gomathi Sankar <ishu27.v@gmail.com>
2022-10-21 16:15:26 +0200

    indexOf

--- src/main/java/se/lexicon/Exercise02.java
+++ src/main/java/se/lexicon/Exercise02.java
@@ -6,11 +6,13 @@ public class Exercise02 {
    * If the element does not exist your method should return -1 as value.
    * Expected output: Index position of number 5 is: 2.
    */
-    public static void ex2() {
+  public static void ex2() {
+    indexOf();
+  }
+  public static void indexOf() {
     int[] elements = {1, 0, 5, 2, 4, 2};
     int number = 5;
     int indexOf = -1;
-
     for(int i = 0; i < elements.length; i++) {
       if(elements[i] == number) {
         indexOf = i;
